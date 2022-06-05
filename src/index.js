4434071fb81eab39cd2ffdaef27b9c5aaeff5eeb
Yuliia2991 <yuliiakurian@gmail.com>
2022-06-05 18:17:10 +0300

    Makes a request and a gallery

---
+++ src/index.js
@@ -0,0 +1,72 @@
+import PhotosApiService from './js/photos-api-service';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const photosApiService = new PhotosApiService();
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  submitBtn: document.querySelector('button'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+
+refs.form.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefault();
+  refs.gallery.innerHTML = '';
+  // gallery.refresh();
+
+  photosApiService.query = e.currentTarget.elements.searchQuery.value;
+  photosApiService.resetPage();
+  photosApiService.fetchPhotos().then(photos => {
+    renderGalleryCards(photos);
+  });
+}
+
+function onLoadMore() {
+  photosApiService.fetchPhotos().then(photos => {
+    renderGalleryCards(photos);
+  });
+}
+
+function renderGalleryCards(photos) {
+  const galleryCardMarkup = photos
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+            <a class="gallery-item" href="${largeImageURL}">
+              <div class="gallery-thumb">
+                <img class="gallery-photo" src="${webformatURL}" alt="${tags}" loading="lazy" />
+              </div>
+            </a>
+            <div class="info">
+              <p class="info-item">
+                <b>Likes</b> ${likes}
+              </p>
+              <p class="info-item">
+                <b>Views</b> ${views}
+              </p>
+              <p class="info-item">
+                <b>Comments</b> ${comments}
+              </p>
+              <p class="info-item">
+                <b>Downloads</b> ${downloads}
+              </p>
+            </div>
+          </div>`
+    )
+    .join('');
+  refs.gallery.insertAdjacentHTML('beforeend', galleryCardMarkup);
+  new SimpleLightbox('.gallery a');
+}
---
+++ src/js/photos-api-service.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '27845155-2bfc883cc65018053cc1f72dd';
+
+export default class PhotosApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchPhotos() {
+    console.log(this);
+    const fetchPhotos = await axios(
+      `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
+    );
+    this.page += 1;
+    return fetchPhotos.data.hits;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
